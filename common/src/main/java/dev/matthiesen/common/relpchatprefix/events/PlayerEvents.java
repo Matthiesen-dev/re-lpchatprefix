877ee111c74008fdbc2758fe51024dcae24ee6c1
Adam Matthiesen <amatthiesen@outlook.com>
2026-06-05 10:48:54 -0700

    Update dependencies and refactor server messaging utility usage in event handlers

--- common/src/main/java/dev/matthiesen/common/relpchatprefix/events/PlayerEvents.java
+++ common/src/main/java/dev/matthiesen/common/relpchatprefix/events/PlayerEvents.java
@@ -1,12 +1,13 @@
 package dev.matthiesen.common.relpchatprefix.events;
 
+import dev.matthiesen.common.matthiesen_lib_api.MatthiesenLibApi;
 import dev.matthiesen.common.matthiesen_lib_api.core.interfaces.MatthiesenLibPlayerEventHandler;
+import dev.matthiesen.common.matthiesen_lib_api.utility.ServerMessagingUtil;
 import dev.matthiesen.common.relpchatprefix.Constants;
 import dev.matthiesen.common.relpchatprefix.ReLPChatPrefix;
 import dev.matthiesen.common.relpchatprefix.config.ModConfig;
 import dev.matthiesen.common.relpchatprefix.data.PlayerStore;
 import dev.matthiesen.common.relpchatprefix.util.Formatter;
-import dev.matthiesen.common.relpchatprefix.util.ServerUtil;
 import net.minecraft.network.chat.Component;
 import net.minecraft.server.MinecraftServer;
 import net.minecraft.server.level.ServerLevel;
@@ -21,7 +22,7 @@ public class PlayerEvents implements MatthiesenLibPlayerEventHandler {
         loginLogoutEvent(player, config.chatOverrides.joinMessage);
 
         try {
-            MinecraftServer server = ServerUtil.getServer();
+            MinecraftServer server = MatthiesenLibApi.getMinecraftServer();
             ServerLevel level = server.overworld();
             PlayerStore store = level.getDataStorage().computeIfAbsent(PlayerStore.FACTORY, Constants.MOD_ID);
             String playerUUID = player.getStringUUID();
@@ -31,7 +32,7 @@ public class PlayerEvents implements MatthiesenLibPlayerEventHandler {
                 if (!config.firstJoin.enable) return;
                 String loginFormat = Formatter.getChatComponent(player, config.firstJoin.message);
                 Component message = Formatter.getMessageComponent(loginFormat);
-                ServerUtil.sendToAllAndConsole(message);
+                ServerMessagingUtil.sendToAllAndConsole(server, message);
 
             }
         } catch (RuntimeException e) {
@@ -48,7 +49,7 @@ public class PlayerEvents implements MatthiesenLibPlayerEventHandler {
         try {
             messageFormat = Formatter.getChatComponent(player, messageFormat);
             Component message = Formatter.getMessageComponent(messageFormat);
-            ServerUtil.sendToAllAndConsole(message);
+            ServerMessagingUtil.sendToAllAndConsole(message);
         } catch (RuntimeException e) {
             Constants.createErrorLog("Error handling player login/logout event for " + player.getName().getString(), e);
         }
--- common/src/main/java/dev/matthiesen/common/relpchatprefix/events/ServerEvents.java
+++ common/src/main/java/dev/matthiesen/common/relpchatprefix/events/ServerEvents.java
@@ -1,10 +1,10 @@
 package dev.matthiesen.common.relpchatprefix.events;
 
+import dev.matthiesen.common.matthiesen_lib_api.utility.ServerMessagingUtil;
 import dev.matthiesen.common.relpchatprefix.Constants;
 import dev.matthiesen.common.relpchatprefix.ReLPChatPrefix;
 import dev.matthiesen.common.relpchatprefix.config.ModConfig;
 import dev.matthiesen.common.relpchatprefix.util.Formatter;
-import dev.matthiesen.common.relpchatprefix.util.ServerUtil;
 import net.minecraft.network.chat.Component;
 import net.minecraft.server.level.ServerPlayer;
 
@@ -15,7 +15,7 @@ public class ServerEvents {
             String messageFormat = Formatter.getChatComponent(player, config.mainConfig.messageFormat);
             Component messageComponent = Formatter.processUserMessage(rawText, config.mainConfig.messageColor);
             Component finalComponent = Formatter.getMessageComponent(messageFormat, messageComponent);
-            ServerUtil.sendToAllAndConsole(finalComponent);
+            ServerMessagingUtil.sendToAllAndConsole(finalComponent);
         } catch (RuntimeException e) {
             Constants.createErrorLog("Error handling server chat event for " + player.getName().getString(), e);
         }
