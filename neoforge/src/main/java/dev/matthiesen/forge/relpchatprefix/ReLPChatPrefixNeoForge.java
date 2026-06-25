807bca8099cea68d4078dbd7e47edf9966d6c331
Adam Matthiesen <amatthiesen@outlook.com>
2026-06-24 17:24:29 -0700

    Add configurable text parser support in ModConfig and ReLPChatPrefix (#6)
    * Add configurable text parser support in ModConfig and ReLPChatPrefix
    
    * Add Embers Text Parser support and update dependencies
    
    * Add optional Ember's Text API dependency to publish-release.yml
    
    * Add optional Ember's Text API dependency to neoforge.mods.toml
    
    * Update matthiesen_lib version to 1.9.1 in configuration files

--- neoforge/src/main/java/dev/matthiesen/forge/relpchatprefix/ReLPChatPrefixNeoForge.java
+++ neoforge/src/main/java/dev/matthiesen/forge/relpchatprefix/ReLPChatPrefixNeoForge.java
@@ -1,7 +1,10 @@
 package dev.matthiesen.forge.relpchatprefix;
 
+import dev.matthiesen.common.matthiesen_lib_api.MatthiesenLibApi;
+import dev.matthiesen.common.matthiesen_lib_api.core.interfaces.MatthiesenLibBuiltInTextParsers;
 import dev.matthiesen.common.relpchatprefix.ReLPChatPrefix;
 import dev.matthiesen.common.relpchatprefix.Constants;
+import dev.matthiesen.forge.relpchatprefix.text_parser.MatthiesenLibEmbersTextParserNeoForge;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.common.Mod;
 import net.neoforged.neoforge.common.NeoForge;
@@ -13,6 +16,14 @@ public class ReLPChatPrefixNeoForge {
         Constants.createInfoLog("Loading for NeoForge Mod Loader");
         ReLPChatPrefix.initialize();
         NeoForge.EVENT_BUS.register(this);
+
+        // If Ember's Text API is loaded, but MatthiesenLib (The Client package) is not installed, let's make sure Ember's is available
+        if (
+                MatthiesenLibApi.isModLoaded(MatthiesenLibBuiltInTextParsers.EMBER.getName()) &&
+                        !MatthiesenLibApi.isModLoaded("matthiesen_lib")
+        ) {
+            MatthiesenLibApi.registerTextParser(new MatthiesenLibEmbersTextParserNeoForge());
+        }
     }
 
     @SubscribeEvent
