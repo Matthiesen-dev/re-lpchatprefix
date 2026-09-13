7ca6ca9e8dcb3e3c6dbdfe4b92f6221e541bdb6f
Adam Matthiesen <amatthiesen@outlook.com>
2026-09-12 20:38:00 -0700

    Add client-side check for NeoForge Mod Loader initialization (#11)

--- neoforge/src/main/java/dev/matthiesen/relpchatprefix/neoforge/ReLPChatPrefixNeoForge.java
+++ neoforge/src/main/java/dev/matthiesen/relpchatprefix/neoforge/ReLPChatPrefixNeoForge.java
@@ -2,13 +2,18 @@ package dev.matthiesen.relpchatprefix.neoforge;
 
 import dev.matthiesen.relpchatprefix.common.ReLPChatPrefix;
 import net.neoforged.fml.common.Mod;
+import net.neoforged.fml.loading.FMLEnvironment;
 
 @Mod(ReLPChatPrefix.MOD_ID)
 public final class ReLPChatPrefixNeoForge {
     public static final ReLPChatPrefix INSTANCE = ReLPChatPrefix.INSTANCE;
 
     public ReLPChatPrefixNeoForge() {
-        INSTANCE.createInfoLog("Loading for NeoForge Mod Loader");
-        INSTANCE.initialize();
+        if (FMLEnvironment.dist.isClient()) {
+            INSTANCE.createInfoLog("Skipping loading for NeoForge Mod Loader. (Client side note supported)");
+        } else {
+            INSTANCE.createInfoLog("Loading for NeoForge Mod Loader");
+            INSTANCE.initialize();
+        }
     }
 }
